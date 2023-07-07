e86dd9a0ad6477824de684d8b19f9bb063943cf8
Dev Raj R <dev.raj.r1729@gmail.com>
2023-07-07 22:15:23 +0530

    added request logger

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, _res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({ msg: "Hello World" });
 });
