d38b01587c53b4a1f109d25ae18cc064e82f18c9
Dev Raj R <dev.raj.r1729@gmail.com>
2023-07-08 06:53:43 +0530

    added connection to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 
 const workoutRoutes = require("./routes/workouts");
 
@@ -15,6 +16,13 @@ app.use((req, _res, next) => {
 
 app.use("/api/workouts", workoutRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("app listening on port", process.env.PORT);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB & app listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
